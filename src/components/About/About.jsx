2732ff51a5484aab514dcd08dfe8186d8b98398d
Hamadn <nhamad919@gmail.com>
2024-01-08 11:03:47 +0400

    About page

---
+++ src/components/About/About.jsx
@@ -0,0 +1,18 @@
+import Content from "./Content";
+import Features from "./Features";
+import Stats from "./Stats";
+import Team from "./Team";
+import Testimonial from "./Testimonial";
+function About() {
+  return (
+    <>
+      <Content />
+      <Features />
+      <Stats />
+      <Team />
+      <Testimonial />
+    </>
+  );
+}
+
+export default About;
