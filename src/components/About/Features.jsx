8a0f8144fc0b0d8f905f505076c56fa8ed923352
Hamadn <nhamad919@gmail.com>
2024-01-08 11:01:59 +0400

    Features section component for about page

---
+++ src/components/About/Features.jsx
@@ -0,0 +1,213 @@
+import Container from "../container/Container";
+
+function Features() {
+  return (
+    <section className="bg-gray-50 text-black">
+      <Container>
+        <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
+          <div className="mx-auto max-w-xl text-center">
+            <h2 className="text-3xl font-bold sm:text-4xl">
+              We Are Ensuring The Best Customer Experience
+            </h2>
+          </div>
+
+          <div className="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+            <a
+              className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-red-700/50 hover:shadow-red-700/50"
+              href="/services/digital-campaigns"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-10 w-10 text-red-700"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path d="M12 14l9-5-9-5-9 5 9 5z" />
+                <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                />
+              </svg>
+
+              <h2 className="mt-4 text-xl font-bold text-black">
+                Most Flexible Payment
+              </h2>
+
+              <p className="mt-1 text-sm text-black">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut
+                quo possimus adipisci distinctio alias voluptatum blanditiis
+                laudantium.
+              </p>
+            </a>
+
+            <a
+              className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-red-700/50 hover:shadow-red-700/50"
+              href="/services/digital-campaigns"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-10 w-10 text-red-700"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path d="M12 14l9-5-9-5-9 5 9 5z" />
+                <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                />
+              </svg>
+
+              <h2 className="mt-4 text-xl font-bold text-black">
+                Personalized Search
+              </h2>
+
+              <p className="mt-1 text-sm text-black">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut
+                quo possimus adipisci distinctio alias voluptatum blanditiis
+                laudantium.
+              </p>
+            </a>
+
+            <a
+              className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-red-700/50 hover:shadow-red-700/50"
+              href="/services/digital-campaigns"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-10 w-10 text-red-700"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path d="M12 14l9-5-9-5-9 5 9 5z" />
+                <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                />
+              </svg>
+
+              <h2 className="mt-4 text-xl font-bold text-black">
+                Valuable Insights
+              </h2>
+
+              <p className="mt-1 text-sm text-black">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut
+                quo possimus adipisci distinctio alias voluptatum blanditiis
+                laudantium.
+              </p>
+            </a>
+
+            <a
+              className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-red-700/50 hover:shadow-red-700/50"
+              href="/services/digital-campaigns"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-10 w-10 text-red-700"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path d="M12 14l9-5-9-5-9 5 9 5z" />
+                <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                />
+              </svg>
+
+              <h2 className="mt-4 text-xl font-bold text-black">
+                Online Car Appraisal
+              </h2>
+
+              <p className="mt-1 text-sm text-black">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut
+                quo possimus adipisci distinctio alias voluptatum blanditiis
+                laudantium.
+              </p>
+            </a>
+
+            <a
+              className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-red-700/50 hover:shadow-red-700/50"
+              href="/services/digital-campaigns"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-10 w-10 text-red-700"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path d="M12 14l9-5-9-5-9 5 9 5z" />
+                <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                />
+              </svg>
+
+              <h2 className="mt-4 text-xl font-bold text-black">
+                Costumer-First Mentality
+              </h2>
+
+              <p className="mt-1 text-sm text-black">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut
+                quo possimus adipisci distinctio alias voluptatum blanditiis
+                laudantium.
+              </p>
+            </a>
+
+            <a
+              className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-red-700/50 hover:shadow-red-700/50"
+              href="/services/digital-campaigns"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-10 w-10 text-red-700"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path d="M12 14l9-5-9-5-9 5 9 5z" />
+                <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                />
+              </svg>
+
+              <h2 className="mt-4 text-xl font-bold text-black">
+                Non-Stop Innovation
+              </h2>
+
+              <p className="mt-1 text-sm text-black">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut
+                quo possimus adipisci distinctio alias voluptatum blanditiis
+                laudantium.
+              </p>
+            </a>
+          </div>
+        </div>
+      </Container>
+    </section>
+  );
+}
+
+export default Features;
